3e56e7aba3d7114415e0d1704e57f11d832b169b
Marceau_Julieron <julieron.mar@gmail.com>
2023-12-01 11:47:36 +0100

    fetchjson

--- my-app/features/api.ts
+++ my-app/features/api.ts
@@ -17,39 +17,35 @@ export const api = createApi({
     }),
 
     fetchAmazonFilms: builder.query<Film[], { Film: string; StreamingService: StreamingService.AmazonPrime }>({
-      query: ({ Film, StreamingService }) => `URL Amazon`,
+      query: () => `https://api.themoviedb.org/3/discover/movie?api_key=6383b6e3ace31d1ff86f07bddd32d91c&language=fr-FR&sort_by=primary_release_date.desc&page=1&with_watch_providers=10&watch_region=FR`,
     }),
 
     fetchNetflixFilms: builder.query<Film[], { Film: string; StreamingService: StreamingService.Netflix }>({
-      query: ({ Film, StreamingService }) => `URL Netflix`,
+      query: () => `https://api.themoviedb.org/3/discover/movie?api_key=6383b6e3ace31d1ff86f07bddd32d91c&language=fr-FR&sort_by=primary_release_date.desc&page=1&with_watch_providers=8&watch_region=FR`,
     }),
 
     fetchAmazonSeries: builder.query<Series[], { Series: string; StreamingService: StreamingService.AmazonPrime }>({
-      query: ({ Series, StreamingService }) => `URL_API_CATALOGUE_`,
+      query: () => `https://api.themoviedb.org/3/discover/tv?api_key=6383b6e3ace31d1ff86f07bddd32d91c&language=fr-FR&sort_by=primary_release_date.desc&page=1&with_watch_providers=119&watch_region=FR&without_genres=16`,
     }),
 
     fetchNetflixSeries: builder.query<Series[], { Series: string; StreamingService: StreamingService.Netflix }>({
-      query: ({ Series, StreamingService }) => `URL_API_CATALOGUE_`,
+      query: () => `https://api.themoviedb.org/3/discover/tv?api_key=6383b6e3ace31d1ff86f07bddd32d91c&language=fr-FR&sort_by=primary_release_date.desc&page=1&with_watch_providers=8&watch_region=FR&without_genres=16`,
     }),
 
     fetchAmazonAnimated: builder.query<Animated[], { Animated: string; StreamingService: StreamingService.AmazonPrime }>({
-      query: ({ Animated, StreamingService }) => `URL_API_CATALOGUE_`,
+      query: () => `https://api.themoviedb.org/3/discover/tv?api_key=6383b6e3ace31d1ff86f07bddd32d91c&language=fr-FR&sort_by=primary_release_date.desc&with_watch_providers=119&watch_region=FR&with_genres=16`,
     }),
 
     fetchNetflixAnimated: builder.query<Animated[], { Animated: string; StreamingService: StreamingService.Netflix }>({
-      query: ({ Animated, StreamingService }) => `URL_API_CATALOGUE_`,
+      query: () => `https://api.themoviedb.org/3/discover/tv?api_key=6383b6e3ace31d1ff86f07bddd32d91c&language=fr-FR&sort_by=primary_release_date.desc&with_watch_providers=8&watch_region=FR&with_genres=16`,
     }),
 
     fetchUpcomingSeries: builder.query<Series[], string>({
-      query: (Series) => `URL_API_FUTURES_SORTIES`,
+      query: () => `https://api.themoviedb.org/3/tv/on_the_air?api_key=6383b6e3ace31d1ff86f07bddd32d91c&language=fr-FR&page=1`,
     }),
 
     fetchUpcomingFilms: builder.query<Film[], string>({
-      query: (Film) => `URL_API_FUTURES_SORTIES_`,
-    }),
-
-    fetchUpcomingAnimated: builder.query<Animated[], string>({
-      query: (Animated) => `URL_API_FUTURES_SORTIES`,
+      query: () => `Uhttps://api.themoviedb.org/3/movie/upcoming?api_key=6383b6e3ace31d1ff86f07bddd32d91c&language=fr-FR&page=1`,
     }),
 
     fetchSearchResults: builder.query<Film[] | Series[] | Animated[], string>({
@@ -73,7 +69,6 @@ export const {
   useFetchNetflixAnimatedQuery,
   useFetchUpcomingSeriesQuery,
   useFetchUpcomingFilmsQuery,
-  useFetchUpcomingAnimatedQuery,
   useFetchSearchResultsQuery,
   useFetchContentDetailsQuery,
 } = api;
