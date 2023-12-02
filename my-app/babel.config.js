65571729e564dd1a81c629100a33879ce0c09e8b
godillar <gabriel.odillard@outlook.fr>
2023-12-02 17:15:20 +0100

    test

--- my-app/babel.config.js
+++ my-app/babel.config.js
@@ -1,10 +1,15 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      // Required for expo-router
-      'expo-router/babel',
+    presets: [
+      [
+        "babel-preset-expo",
+        {
+          // Use React 17 automatic JSX runtime.
+          jsxRuntime: "automatic",
+        },
+      ],
     ],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
