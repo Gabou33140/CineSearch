46ed06185a36879e71de3cc8b4e08cf53eb47841
godillar <gabriel.odillard@outlook.fr>
2023-12-04 09:33:27 +0100

    update header.tsx

--- my-app/components/Header.tsx
+++ my-app/components/Header.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
 
 const Header = () => {
+  const categories = ['Films', 'Séries', 'Animés'];
   const [selectedCategory, setSelectedCategory] = useState(null);
 
   const handleCategorySelect = (category) => {
@@ -14,22 +15,57 @@ const Header = () => {
     }
   };
 
-  return (
-    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 16, backgroundColor: 'black' }}>
-      <Image source={require('../assets/images/logo_cinesearch.png')} style={{ width: 32, height: 32 }} />
-      <View style={{ flexDirection: 'row' }}>
-        <TouchableOpacity onPress={() => handleCategorySelect('Films')}>
-          <Text style={{ color: selectedCategory === 'Films' ? 'red' : 'white', marginRight: 16 }}>Films</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleCategorySelect('Séries')}>
-          <Text style={{ color: selectedCategory === 'Séries' ? 'red' : 'white', marginRight: 16 }}>Séries</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleCategorySelect('Animés')}>
-          <Text style={{ color: selectedCategory === 'Animés' ? 'red' : 'white' }}>Animés</Text>
-        </TouchableOpacity>
+  const renderDropDownMenu = () => {
+    if (!selectedCategory) {
+      return null;
+    }
+
+    // Vous pouvez personnaliser le contenu du menu déroulant ici
+    // Pour l'instant, nous utilisons une simple FlatList de démonstration
+    return (
+      <View style={styles.dropDownMenu}>
+        <FlatList
+          data={['Netflix', 'Prime Vidéo', 'Tous', 'A venir']}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => console.log(`Selected: ${item}`)}>
+              <Text style={styles.dropDownMenuItem}>{item}</Text>
+            </TouchableOpacity>
+          )}
+        />
       </View>
+    );
+  };
+
+  return (
+    <View style={{ flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', padding: 16, backgroundColor: 'black' }}>
+      <Image source={require('../assets/images/logo_cinesearch.png')} style={{ width: 32, height: 32, marginBottom: 16 }} />
+      <View style={{ flexDirection: 'row' }}>
+        {categories.map((category) => (
+          <TouchableOpacity key={category} onPress={() => handleCategorySelect(category)}>
+            <Text style={{ color: selectedCategory === category ? 'red' : 'white', marginRight: 16 }}>{category}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      {renderDropDownMenu()}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  dropDownMenu: {
+    position: 'absolute',
+    top: 80,
+    left: 16,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    elevation: 5,
+    padding: 8,
+  },
+  dropDownMenuItem: {
+    fontSize: 16,
+    paddingVertical: 8,
+  },
+});
 
 export default Header;
