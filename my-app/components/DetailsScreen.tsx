f80c568444b243fd226aa1993afc32a4dadf7985
godillar <gabriel.odillard@outlook.fr>
2023-12-07 15:42:50 +0100

    last version details ok

--- my-app/components/DetailsScreen.tsx
+++ my-app/components/DetailsScreen.tsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { ScrollView, View } from 'react-native';
+import React from 'react';
+import { ScrollView, View, Image } from 'react-native';
 import { useFetchContentDetailsByIdQuery } from '../features/api';
 import { Text} from '../components/Themed';
-import { Film, Series, Animated } from '../types/types';
+import { Film, Series } from '../types/types';
 
 interface ContentDetailsProps {
   id: number;
 }
 
 interface DisplayDetailsProps {
-  contentType: 'tv' | 'movie';
-  content: Film | Series | Animated;
+  contentType: 'Serie' | 'Film' | 'Animé';
+  content: Film | Series | undefined ;
 }
 
 const ContentDetails: React.FC<ContentDetailsProps> = ({ id }) => {
-  const { data: dataMovie, isLoading: isMovieLoading } = useFetchContentDetailsByIdQuery({ id: id, contentType: 'movie' });
-  const { data: dataSeries, isLoading: isTVLoading }= useFetchContentDetailsByIdQuery({ id: id, contentType: 'tv' });
+  const { data: dataMovie, isLoading: isMovieLoading } = useFetchContentDetailsByIdQuery({ id: id, contentType: 'Film' });
+  const { data: dataSeries, isLoading: isTVLoading }= useFetchContentDetailsByIdQuery({ id: id, contentType: 'Serie' });
   // Render les détails du contenu dans votre composant
   return (
     <View>
@@ -24,11 +24,13 @@ const ContentDetails: React.FC<ContentDetailsProps> = ({ id }) => {
       ) : (
         console.log(dataMovie || dataSeries),
         <View>
-          <Text> bonjour </Text>
-          {/* <Text>{dataMovie ? <DisplayDetails contentType: 'movie' /> : ''}</Text>
-          <Text>{dataSeries ? <DisplayDetails contentType: 'tv' /> : ''}</Text> */}
+
+          {dataMovie || dataSeries ? 
+          <DisplayDetails contentType={dataMovie ? 'Film' : 'Serie'} content={dataMovie || dataSeries} /> 
+          : <Text>Aucuns détails disponibles</Text>}
+
           <Text>{(dataSeries as Series)?.nbrSeason || ''}</Text>
-          <Text>{ !dataSeries && !dataMovie ? 'Aucuns détails disponibles' : ''}</Text>
+
 
         </View>
       )}
@@ -37,39 +39,50 @@ const ContentDetails: React.FC<ContentDetailsProps> = ({ id }) => {
 };
 
 
-// const DisplayDetails: React.FC<DisplayDetailsProps> = ({ contentType, content }) => {
-//   return (
-//     <ScrollView>
-//       <Text style={{ fontSize: 24, fontWeight: 'bold', textAlign: 'center', marginVertical: 10 }}>
-//         {contentType === 'tv' ? 'Animés' : 'Films'}
-//       </Text>
-//       <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center', marginBottom: 10 }}>
-//         {content.name}
-//       </Text>
-//       <Image
-//         source={{ uri: content.poster_path || '' }}
-//         style={{ width: '100%', height: 300, resizeMode: 'cover', marginBottom: 10 }}
-//       />
-//       <View style={{ marginHorizontal: 20, marginBottom: 10 }}>
-//         <Text style={{ fontSize: 16, marginBottom: 10 }}>{content.synopsis}</Text>
-//         {contentType === 'tv' && (
-//           <>
-//             <Text style={{ fontSize: 16, marginBottom: 10 }}>
-//               Nombre de saisons: {(content as Series).number_of_seasons}
-//             </Text>
-//             <Text style={{ fontSize: 16, marginBottom: 10 }}>
-//               Nombre d'épisodes: {(content as Series).number_of_episodes}
-//             </Text>
-//           </>
-//         )}
-//         {contentType === 'movie' && (
-//           <Text style={{ fontSize: 16, marginBottom: 10 }}>
-//             Durée: {(content as Film).duration} minutes
-//           </Text>
-//         )}
-//       </View>
-//     </ScrollView>
-//   );
-// };
+const DisplayDetails: React.FC<DisplayDetailsProps> = ({ contentType, content }) => {
+
+  if (contentType === 'Serie') {
+    const genreIsAnimation = content?.genre.some((genre) => genre.id === 16);
+    if (genreIsAnimation) {
+      contentType = "Animé";
+    }
+  else
+    contentType = "Film"
+  }
+
+
+  return (
+    <ScrollView>
+      <Text style={{ fontSize: 24, fontWeight: 'bold', textAlign: 'center', marginVertical: 10 }}>
+        {contentType}
+      </Text>
+      <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center', marginBottom: 10 }}>
+        {content?.name}
+      </Text>
+      <Image
+        source={{ uri: `https://image.tmdb.org/t/p/original/${content?.poster_path}`}}
+        style={{ width: '100%', height: 300, resizeMode: 'cover', marginBottom: 10 }}
+      />
+      <View style={{ marginHorizontal: 20, marginBottom: 10 }}>
+        <Text style={{ fontSize: 16, marginBottom: 10 }}>{content?.synopsis}</Text>
+        {contentType === 'Serie' && (
+          <>
+            <Text style={{ fontSize: 16, marginBottom: 10 }}>
+              Nombre de saisons: {(content as Series).nbrSeason}
+            </Text>
+            <Text style={{ fontSize: 16, marginBottom: 10 }}>
+              Nombre d'épisodes: {(content as Series).nbrEpisode}
+            </Text>
+          </>
+        )}
+        {contentType === 'Film' && (
+          <Text style={{ fontSize: 16, marginBottom: 10 }}>
+            Durée: {(content as Film).duration} minutes
+          </Text>
+        )}
+      </View>
+    </ScrollView>
+  );
+};
 
 export default ContentDetails;
