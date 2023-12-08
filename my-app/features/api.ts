1f7bc8f27b353b3fa9b3710c3619712688bf7ac6
godillar <gabriel.odillard@outlook.fr>
2023-12-08 10:47:24 +0100

    searchbar pas encore fonctionnel

--- my-app/features/api.ts
+++ my-app/features/api.ts
@@ -1,194 +1,160 @@
 import { FetchBaseQueryMeta, createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import  { Film, Series, Animated, StreamingService, Content, MinimalContent } from "../types/types";
+import  { Film, Series, Animated, searchResults, StreamingService, Content, MinimalContent } from "../types/types";
+
+const api_key = "6383b6e3ace31d1ff86f07bddd32d91c"
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3/' }),
   endpoints: (builder) => ({
     fetchTrendingSeries: builder.query<MinimalContent[], void>({
-      query: () => `discover/tv?api_key=6383b6e3ace31d1ff86f07bddd32d91c&include_adult=false&include_null_first_air_dates=false&language=fr-FR&page=1&sort_by=popularity.desc&without_genres=16`,
+      query: () => `discover/tv?api_key=${api_key}&include_adult=false&include_null_first_air_dates=false&language=fr-FR&page=1&sort_by=popularity.desc&without_genres=16`,
       transformResponse: (response: {results: Series[]}, meta: FetchBaseQueryMeta | undefined, arg: void): MinimalContent[] | Promise<MinimalContent[]> => {
-        // Ici, tu peux manipuler la réponse selon tes besoins
-        // Par exemple, sélectionner uniquement certaines propriétés
         return response.results.map((Series) => ({
           id: Series.id,
           name: Series.name,
           poster_path: Series.poster_path || '',
-          // ... autres propriétés que tu veux inclure
         }));
       },
     }),
 
     fetchTrendingFilms: builder.query<MinimalContent[], void>({
-      query: () => `trending/movie/week?api_key=6383b6e3ace31d1ff86f07bddd32d91c&language=fr-FR`,
+      query: () => `trending/movie/week?api_key=${api_key}&language=fr-FR`,
       transformResponse: (response: {results: Film[]}, meta: FetchBaseQueryMeta | undefined, arg: void): MinimalContent[] | Promise<MinimalContent[]> => {
-        // Ici, tu peux manipuler la réponse selon tes besoins
-        // Par exemple, sélectionner uniquement certaines propriétés
-        // console.log(response.results)
         return response.results.map((Film) => ({
           id: Film.id,
           name: Film.title,
           poster_path: Film.poster_path || '',
-          // ... autres propriétés que tu veux inclure
         }));
       },
     }),
 
     fetchTrendingAnimated: builder.query<MinimalContent[], void>({
-      query: () => `discover/tv?api_key=6383b6e3ace31d1ff86f07bddd32d91c&include_adult=false&include_null_first_air_dates=false&language=fr-FR&page=1&sort_by=popularity.desc&with_genres=16`,
+      query: () => `discover/tv?api_key=${api_key}&include_adult=false&include_null_first_air_dates=false&language=fr-FR&page=1&sort_by=popularity.desc&with_genres=16`,
       transformResponse: (response: {results: Animated[]}, meta: FetchBaseQueryMeta | undefined, arg: void): MinimalContent[] | Promise<MinimalContent[]> => {
-        // Ici, tu peux manipuler la réponse selon tes besoins
-        // Par exemple, sélectionner uniquement certaines propriétés
         return response.results.map((Animated) => ({
           id: Animated.id,
           name: Animated.name,
           poster_path: Animated.poster_path || '',
-          // ... autres propriétés que tu veux inclure
         }));
       },
     }),
 
     fetchAmazonFilms: builder.query<MinimalContent[], void>({
-      query: () => `discover/movie?api_key=6383b6e3ace31d1ff86f07bddd32d91c&language=fr-FR&sort_by=primary_release_date.desc&page=1&with_watch_providers=119&watch_region=FR`,
+      query: () => `discover/movie?api_key=${api_key}&language=fr-FR&sort_by=primary_release_date.desc&page=1&with_watch_providers=119&watch_region=FR`,
       transformResponse: (response: {results: Film[]}, meta: FetchBaseQueryMeta | undefined, arg: void): MinimalContent[] | Promise<MinimalContent[]> => {
-        // Ici, tu peux manipuler la réponse selon tes besoins
-        // Par exemple, sélectionner uniquement certaines propriétés
         return response.results.map((Film) => ({
           id: Film.id,
           name: Film.title,
           poster_path: Film.poster_path || '',
-          // ... autres propriétés que tu veux inclure
         }));
       },
     }),
 
     fetchNetflixFilms: builder.query<MinimalContent[], void>({
-      query: () => `discover/movie?api_key=6383b6e3ace31d1ff86f07bddd32d91c&language=fr-FR&sort_by=primary_release_date.desc&page=1&with_watch_providers=8&watch_region=FR`,
+      query: () => `discover/movie?api_key=${api_key}&language=fr-FR&sort_by=primary_release_date.desc&page=1&with_watch_providers=8&watch_region=FR`,
       transformResponse: (response: {results: Film[]}, meta: FetchBaseQueryMeta | undefined, arg: void): MinimalContent[] | Promise<MinimalContent[]> => {
-        // Ici, tu peux manipuler la réponse selon tes besoins
-        // Par exemple, sélectionner uniquement certaines propriétés
         return response.results.map((Film) => ({
           id: Film.id,
           name: Film.title,
           poster_path: Film.poster_path || '',
-          // ... autres propriétés que tu veux inclure
         }));
       },
     }),
 
     fetchAmazonSeries: builder.query<MinimalContent[], void>({
-      query: () => `discover/tv?api_key=6383b6e3ace31d1ff86f07bddd32d91c&include_adult=false&include_null_first_air_dates=false&language=fr-FR&page=1&sort_by=popularity.desc&with_watch_providers=119&without_genres=16`,
+      query: () => `discover/tv?api_key=${api_key}&include_adult=false&include_null_first_air_dates=false&language=fr-FR&page=1&sort_by=popularity.desc&with_watch_providers=119&without_genres=16`,
       transformResponse: (response: {results: Series[]}, meta: FetchBaseQueryMeta | undefined, arg: void): MinimalContent[] | Promise<MinimalContent[]> => {
-        // Ici, tu peux manipuler la réponse selon tes besoins
-        // Par exemple, sélectionner uniquement certaines propriétés
         return response.results.map((Series) => ({
           id: Series.id,
           name: Series.name,
           poster_path: Series.poster_path || '',
-          // ... autres propriétés que tu veux inclure
         }));
       },
     }),
 
     fetchNetflixSeries: builder.query<MinimalContent[], void>({
-      query: () => `discover/tv?api_key=6383b6e3ace31d1ff86f07bddd32d91c&include_adult=false&include_null_first_air_dates=false&language=fr-FR&page=1&sort_by=popularity.desc&with_watch_providers=8&without_genres=16`,
+      query: () => `discover/tv?api_key=${api_key}&include_adult=false&include_null_first_air_dates=false&language=fr-FR&page=1&sort_by=popularity.desc&with_watch_providers=8&without_genres=16`,
       transformResponse: (response: {results: Series[]}, meta: FetchBaseQueryMeta | undefined, arg: void): MinimalContent[] | Promise<MinimalContent[]> => {
-        // Ici, tu peux manipuler la réponse selon tes besoins
-        // Par exemple, sélectionner uniquement certaines propriétés
         return response.results.map((Series) => ({
           id: Series.id,
           name: Series.name,
           poster_path: Series.poster_path || '',
-          // ... autres propriétés que tu veux inclure
         }));
       },
     }),
 
     fetchAmazonAnimated: builder.query<MinimalContent[], void>({
-      query: () => `discover/tv?api_key=6383b6e3ace31d1ff86f07bddd32d91c&language=fr-FR&sort_by=primary_release_date.desc&with_watch_providers=119&watch_region=FR&with_genres=16`,
+      query: () => `discover/tv?api_key=${api_key}&language=fr-FR&sort_by=primary_release_date.desc&with_watch_providers=119&watch_region=FR&with_genres=16`,
       transformResponse: (response: {results: Animated[]}, meta: FetchBaseQueryMeta | undefined, arg: void): MinimalContent[] | Promise<MinimalContent[]> => {
-        // Ici, tu peux manipuler la réponse selon tes besoins
-        // Par exemple, sélectionner uniquement certaines propriétés
         return response.results.map((Animated) => ({
           id: Animated.id,
           name: Animated.name,
           poster_path: Animated.poster_path || '',
-          // ... autres propriétés que tu veux inclure
         }));
       },
     }),
 
     fetchNetflixAnimated: builder.query<MinimalContent[], void>({
-      query: () => `discover/tv?api_key=6383b6e3ace31d1ff86f07bddd32d91c&include_adult=false&include_null_first_air_dates=false&language=fr-FR&page=1&sort_by=popularity.desc&with_genres=16&with_watch_providers=8`,
+      query: () => `discover/tv?api_key=${api_key}&include_adult=false&include_null_first_air_dates=false&language=fr-FR&page=1&sort_by=popularity.desc&with_genres=16&with_watch_providers=8`,
       transformResponse: (response: {results: Animated[]}, meta: FetchBaseQueryMeta | undefined, arg: void): MinimalContent[] | Promise<MinimalContent[]> => {
-        // Ici, tu peux manipuler la réponse selon tes besoins
-        // Par exemple, sélectionner uniquement certaines propriétés
         return response.results.map((Animated) => ({
           id: Animated.id,
           name: Animated.name,
           poster_path: Animated.poster_path || '',
-          // ... autres propriétés que tu veux inclure
         }));
       },
     }),
 
     fetchUpcomingSeries: builder.query<MinimalContent[], void>({
-      query: () => `tv/on_the_air?api_key=6383b6e3ace31d1ff86f07bddd32d91c&language=fr-FR&page=1`,
+      query: () => `tv/on_the_air?api_key=${api_key}&language=fr-FR&page=1`,
       transformResponse: (response: {results: Series[], meta: FetchBaseQueryMeta | undefined, arg: void}): MinimalContent[] | Promise<MinimalContent[]> => {
-        // Ici, tu peux manipuler la réponse selon tes besoins
-        // Par exemple, sélectionner uniquement certaines propriétés
         return response.results.map((Series) => ({
           id: Series.id,
           name: Series.name,
           poster_path: Series.poster_path || '',
-          // ... autres propriétés que tu veux inclure
         }));
       },
     }),
 
     fetchUpcomingFilms: builder.query<MinimalContent[], void>({
-      query: () => `movie/upcoming?api_key=6383b6e3ace31d1ff86f07bddd32d91c&language=fr-FR&page=1`,
+      query: () => `movie/upcoming?api_key=${api_key}&language=fr-FR&page=1`,
       transformResponse: (response: { results: Film[] }, meta: FetchBaseQueryMeta | undefined, arg: void): MinimalContent[] | Promise<MinimalContent[]> => {
-        // Ici, tu peux manipuler la réponse selon tes besoins
-        // Par exemple, sélectionner uniquement certaines propriétés
         return response.results.map((Film) => ({
           id: Film.id,
           name: Film.title,
           poster_path: Film.poster_path || '',
-          // ... autres propriétés que tu veux inclure
         }));
       },
     }),
 
     fetchUpcomingAnimated: builder.query<MinimalContent[], void>({
-      query: () => `Umovie/upcoming?api_key=6383b6e3ace31d1ff86f07bddd32d91c&language=fr-FR&page=1`,
+      query: () => `Umovie/upcoming?api_key=${api_key}&language=fr-FR&page=1`,
       transformResponse: (response: Animated[], meta: FetchBaseQueryMeta | undefined, arg: void): MinimalContent[] | Promise<MinimalContent[]> => {
-        // Ici, tu peux manipuler la réponse selon tes besoins
-        // Par exemple, sélectionner uniquement certaines propriétés
         return response.map((Animated) => ({
           id: Animated.id,
           name: Animated.name,
           poster_path: Animated.poster_path,
-          // ... autres propriétés que tu veux inclure
         }));
       },
     }),
 
-    fetchSearchResults: builder.query<Content[], string >({
-      query: (searchTerm) => 'multi?api_key=6383b6e3ace31d1ff86f07bddd32d91c&query=${searchTerm}&include_adult=false&language=fr-FR&page=1',
-      transformResponse: (response: Content[], meta: FetchBaseQueryMeta | undefined, arg: string ): Content[] | Promise<Content[]> => {
-        // Ici, tu peux manipuler la réponse selon tes besoins
-        // Par exemple, sélectionner uniquement certaines propriétés
-        return response.map((Content) => ({
-          id: Content.id,
-          name: Content.name,
-          poster_path: Content.poster_path,
-          // ... autres propriétés que tu veux inclure
-        }));
+    fetchSearchResults: builder.query<searchResults, { searchTerm: string }>({
+      query: (searchTerm) => `search/?api_key=${api_key}&keyword?query=${searchTerm}`,
+      transformResponse: (response: searchResults, meta: FetchBaseQueryMeta | undefined, arg: string): searchResults | Promise<searchResults> => {
+        console.log(response)
+        return {
+          page: response.page,
+          results: response.results.map((result) => ({
+            name: result.name,
+            id: result.id,
+          })),
+        };
       },
     }),
+    
 
     fetchContentDetailsById: builder.query<Film | Series | Animated, { id: number; contentType: 'movie' | 'tv' }>({
-      query: ({ id, contentType }) => `${contentType}/${id}?api_key=6383b6e3ace31d1ff86f07bddd32d91c&language=fr-FR`,
+      query: ({ id, contentType }) => `${contentType}/${id}?api_key=${api_key}&language=fr-FR`,
       transformResponse: (response: any, meta: FetchBaseQueryMeta | undefined, arg: { id: number; contentType: string }): Film | Series | Animated | Promise<Film | Series | Animated> => {
         if (arg.contentType === 'tv') {
           return transformTVResponse(response);
@@ -256,6 +222,7 @@ export const {
   useFetchUpcomingAnimatedQuery,
   useFetchUpcomingFilmsQuery,
   useFetchUpcomingSeriesQuery,
+  useFetchSearchResultsQuery,
   useFetchContentDetailsByIdQuery
 } = api;
 
--- my-app/types/types.ts
+++ my-app/types/types.ts
@@ -1,97 +1,107 @@
 export interface Content {
-        id: number
-        name?: string
-        platform?: string
-        author?: string
-        genre: Genre[]
-        poster_path?: string
-        synopsis?: string
-  }
+      id: number
+      name?: string
+      platform?: string
+      author?: string
+      genre: Genre[]
+      poster_path?: string
+      synopsis?: string
+}
 
 interface Genre {
+id: number;
+name: string;
+}
+
+export interface searchResults {
+  page : number;
+  results: result[];
+}
+
+interface result {
   id: number;
   name: string;
 }
 
-  export interface TrendingContent {
-    id: string;
-    backdrop_path: string;
-  }
-  
-  export interface Animated extends Content {
-    nbrEpisode: number
-    nbrSeason: number
-  }
+export interface TrendingContent {
+  id: string;
+  backdrop_path: string;
+}
 
-  export interface Film extends Content {
-    duration: number;
-  }
+export interface Animated extends Content {
+  nbrEpisode: number
+  nbrSeason: number
+}
 
-  export interface Series extends Content {
-    nbrEpisode: number
-    nbrSeason: number
-  }
+export interface Film extends Content {
+  duration: number;
+}
 
-  export interface TrendingSeriesResponse {
-    results: Series[];
-    // Ajoute d'autres propriétés si nécessaire
-  }
+export interface Series extends Content {
+  nbrEpisode: number
+  nbrSeason: number
+}
 
-  export interface MinimalContent {
-    id: number;
-    name?: string;
-    poster_path: string;
-    title?: string;
-  }
-  
-  export interface CatalogueSeriesResponse {
-    results: Series[];
-    // Ajoute d'autres propriétés si nécessaire
-  }
-  
-  export interface UpcomingSeriesResponse {
-    results: Series[];
-    // Ajoute d'autres propriétés si nécessaire
-  }
-  
-  export interface TrendingFilmResponse {
-    results: Film[];
-    // Ajoute d'autres propriétés si nécessaire
-  }
-  
-  export interface CatalogueFilmResponse {
-    results: Film[];
-    // Ajoute d'autres propriétés si nécessaire
-  }
-  
-  export interface UpcomingFilmResponse {
-    results: Film[];
-    // Ajoute d'autres propriétés si nécessaire
-  }
+export interface TrendingSeriesResponse {
+  results: Series[];
+  // Ajoute d'autres propriétés si nécessaire
+}
 
-  export interface TrendingAnimatedResponse {
-    results: Animated[];
-    // Ajoute d'autres propriétés si nécessaire
-  }
-  
-  export interface CatalogueAnimatedResponse {
-    results: Animated[];
-    // Ajoute d'autres propriétés si nécessaire
-  }
-  
-  export interface UpcomingAnimatedResponse {
-    results: Animated[];
-    // Ajoute d'autres propriétés si nécessaire
-  }
+export interface MinimalContent {
+  id: number;
+  name?: string;
+  poster_path: string;
+  title?: string;
+}
 
-  export interface SearchResultsResponse {
-    results: Content[];
-    // Ajoute d'autres propriétés si nécessaire
-  }
-  
+export interface CatalogueSeriesResponse {
+  results: Series[];
+  // Ajoute d'autres propriétés si nécessaire
+}
 
-  export enum StreamingService {
-    AmazonPrime,
-    Netflix,
-  }
-  
+export interface UpcomingSeriesResponse {
+  results: Series[];
+  // Ajoute d'autres propriétés si nécessaire
+}
+
+export interface TrendingFilmResponse {
+  results: Film[];
+  // Ajoute d'autres propriétés si nécessaire
+}
+
+export interface CatalogueFilmResponse {
+  results: Film[];
+  // Ajoute d'autres propriétés si nécessaire
+}
+
+export interface UpcomingFilmResponse {
+  results: Film[];
+  // Ajoute d'autres propriétés si nécessaire
+}
+
+export interface TrendingAnimatedResponse {
+  results: Animated[];
+  // Ajoute d'autres propriétés si nécessaire
+}
+
+export interface CatalogueAnimatedResponse {
+  results: Animated[];
+  // Ajoute d'autres propriétés si nécessaire
+}
+
+export interface UpcomingAnimatedResponse {
+  results: Animated[];
+  // Ajoute d'autres propriétés si nécessaire
+}
+
+export interface SearchResultsResponse {
+  results: Content[];
+  // Ajoute d'autres propriétés si nécessaire
+}
+
+
+export enum StreamingService {
+  AmazonPrime,
+  Netflix,
+}
+
